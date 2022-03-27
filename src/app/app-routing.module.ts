94f67788a91ac6cc4b123a2a4f17a1d60647b6ab
cbuiles <cgomezb@unal.edu.co>
2022-03-27 13:57:55 -0500

    Clase 9. Como funciona y como crear el Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,32 @@ import { SkeletonComponent } from '@layout/skeleton/skeleton.component';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: '/panel/user',
+    pathMatch: 'full' //Usado para que redirija solo cuando la URL concuerda completamente
+  },
+  {
+    path: 'panel',
     component: SkeletonComponent,
     children: [
       {
-        path: '',
+        path: 'user',
+        // Cargando de forma Lazy para que se vaya cargando mientras la vamos necesitando
         loadChildren: () =>
           import('@modules/user/user.module').then( (m) => m.UserModule)
+      },
+      {
+        //En caso de no coincidir con ninguna ruta , entra aca
+        path: '**',
+        redirectTo: '/panel/user',
+        pathMatch: 'full'
       }
     ]
+  },
+  {
+    //En caso de no coincidir con ninguna ruta , entra aca
+    path: '**',
+    redirectTo: '/panel/user',
+    pathMatch: 'full'
   }
 ];
 
--- src/app/modules/user/user-list/user-list.component.ts
+++ src/app/modules/user/user-list/user-list.component.ts
@@ -9,6 +9,7 @@ export class UserListComponent implements OnInit {
 
   public users: ICardUser[] = [
     {
+      id:1,
       name: 'Ivan Larios',
       age: 27,
       description: 'Desarrollador fullstack',
@@ -16,36 +17,42 @@ export class UserListComponent implements OnInit {
       work: 'Project manager',
     },
     {
+      id:2,
       name: 'Martin Larios',
       age: 19,
       description: 'Diseñador',
       avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg'
     },
     {
+      id:3,
       name: 'Luis Larios',
       age: 27,
       description: 'Desarrollador back',
       avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg'
     },
     {
+      id:4,
       name: 'Hector Larios',
       age: 20,
       description: 'Desarrollador front',
       avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg'
     },
     {
+      id:5,
       name: 'Martin Larios',
       age: 19,
       description: 'Diseñador',
       avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg'
     },
     {
+      id:6,
       name: 'Luis Larios',
       age: 27,
       description: 'Desarrollador back',
       avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg'
     },
     {
+      id:7,
       name: 'Hector Larios',
       age: 20,
       description: 'Desarrollador front',
