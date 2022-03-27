b2af1cd280af939115aec6a61b48233b564ed4a7
cbuiles <cgomezb@unal.edu.co>
2022-03-27 13:11:52 -0500

    Primer Commit

---
+++ src/app/modules/user/user-list/user-list.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { ICardUser } from '@shared/components/cards/card-user/icard-user.metadata';
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss']
+})
+export class UserListComponent implements OnInit {
+
+  public users: ICardUser[] = [
+    {
+      name: 'Ivan Larios',
+      age: 27,
+      description: 'Desarrollador fullstack',
+      avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg',
+      work: 'Project manager',
+    },
+    {
+      name: 'Martin Larios',
+      age: 19,
+      description: 'Diseñador',
+      avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg'
+    },
+    {
+      name: 'Luis Larios',
+      age: 27,
+      description: 'Desarrollador back',
+      avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg'
+    },
+    {
+      name: 'Hector Larios',
+      age: 20,
+      description: 'Desarrollador front',
+      avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg'
+    },
+    {
+      name: 'Martin Larios',
+      age: 19,
+      description: 'Diseñador',
+      avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg'
+    },
+    {
+      name: 'Luis Larios',
+      age: 27,
+      description: 'Desarrollador back',
+      avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg'
+    },
+    {
+      name: 'Hector Larios',
+      age: 20,
+      description: 'Desarrollador front',
+      avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg'
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
